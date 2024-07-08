f5bc2cadc18cced65ec96f3ae892eebddaa770c6
Hanna Komornitzyk <hanna.komornitzyk@gmail.com>
2024-07-08 11:51:57 +0200

    Contact Details adjusted

--- src/pages/ContactDetails.jsx
+++ src/pages/ContactDetails.jsx
@@ -1,14 +1,30 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import styles from "../styles/ContactDetails.module.css";
+import logo from "../assets/logo_name.png"; // Adjust the path to your logo
 
 const ContactDetails = () => {
+  const email = "contact@whereismybird.com";
+  const subject = encodeURIComponent("Hello, where is my bird?!");
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    window.open(`mailto:${email}?subject=${subject}`, "_blank");
+  };
+
   return (
     <div>
       <div className={styles.contactPage}>
-        <h1>Contact Us</h1>
-        <p>Email: contact@whereismybird.com</p>
+        <img src={logo} alt="Company Logo" className={styles.logo} />
+        <h1>Get in touch!</h1>
+        <p>
+          <a
+            href={`mailto:${email}?subject=${subject}`}
+            onClick={handleClick}
+            className={styles.emailLink}
+          >
+            {email}
+          </a>
+        </p>
       </div>
     </div>
   );
