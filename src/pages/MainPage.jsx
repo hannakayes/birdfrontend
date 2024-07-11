e0552ef1c33a09821af3c07a4464ff6174d4396b
Hanna Komornitzyk <hanna.komornitzyk@gmail.com>
2024-07-11 22:18:59 +0200

    Filter for MainPage styled

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -8,7 +8,7 @@ const MainPage = ({ birds, fetchBirds, favorites, onToggleFavorite }) => {
   const [filter, setFilter] = useState("");
   const [filterType, setFilterType] = useState("family");
   const [showDropdown, setShowDropdown] = useState(false);
-  const [isFilterActive, setIsFilterActive] = useState(false); // for clear buttons tracking 
+  const [isFilterActive, setIsFilterActive] = useState(false);
 
   const handleAddBird = () => {
     setShowBirdForm(true);
@@ -61,13 +61,13 @@ const MainPage = ({ birds, fetchBirds, favorites, onToggleFavorite }) => {
   const handleFilterChange = (e) => {
     const value = e.target.value;
     setFilter(value);
-    setIsFilterActive(value !== "" || filterType !== "family"); // for hide the clear button, each one should track the activity
+    setIsFilterActive(value !== "" || filterType !== "family");
   };
 
   const handleFilterTypeChange = (e) => {
     const value = e.target.value;
     setFilterType(value);
-    setIsFilterActive(value !== "" || filter !== ""); 
+    setIsFilterActive(value !== "" || filter !== "");
   };
 
   const toggleDropdown = () => {
@@ -77,7 +77,7 @@ const MainPage = ({ birds, fetchBirds, favorites, onToggleFavorite }) => {
   const clearFilter = () => {
     setFilter("");
     setFilterType("family");
-    setIsFilterActive(false); 
+    setIsFilterActive(false);
   };
 
   const filteredBirds = birds.filter((bird) => {
@@ -120,84 +120,91 @@ const MainPage = ({ birds, fetchBirds, favorites, onToggleFavorite }) => {
               {filterType === "family" && (
                 <>
                   <option value="Accipitridae">Accipitridae</option>
-              <option value="Alcedinidae">Alcedinidae</option>
-              <option value="Anatidae">Anatidae</option>
-              <option value="Apodidae">Apodidae</option>
-              <option value="Ardeidae">Ardeidae</option>
-              <option value="Caprimulgidae">Caprimulgidae</option>
-              <option value="Certhiidae">Certhiidae</option>
-              <option value="Charadriidae">Charadriidae</option>
-              <option value="Ciconiidae">Ciconiidae</option>
-              <option value="Columbidae">Columbidae</option>
-              <option value="Corvidae">Corvidae</option>
-              <option value="Cuculidae">Cuculidae</option>
-              <option value="Falconidae">Falconidae</option>
-              <option value="Fringillidae">Fringillidae</option>
-              <option value="Haematopodidae">Haematopodidae</option>
-              <option value="Hirundinidae">Hirundinidae</option>
-              <option value="Laniidae">Laniidae</option>
-              <option value="Locustellidae">Locustellidae</option>
-              <option value="Meropidae">Meropidae</option>
-              <option value="Motacillidae">Motacillidae</option>
-              <option value="Muscicapidae">Muscicapidae</option>
-              <option value="Oriolidae">Oriolidae</option>
-              <option value="Panuridae">Panuridae</option>
-              <option value="Paridae">Paridae</option>
-              <option value="Phasianidae">Phasianidae</option>
-              <option value="Phylloscopidae">Phylloscopidae</option>
-              <option value="Picidae">Picidae</option>
-              <option value="Podicipedidae">Podicipedidae</option>
-              <option value="Prunellidae">Prunellidae</option>
-              <option value="Regulidae">Regulidae</option>
-              <option value="Remizidae">Remizidae</option>
-              <option value="Scolopacidae">Scolopacidae</option>
-              <option value="Sittidae">Sittidae</option>
-              <option value="Sturnidae">Sturnidae</option>
-              <option value="Sylviidae">Sylviidae</option>
-              <option value="Troglodytidae">Troglodytidae</option>
-              <option value="Turdidae">Turdidae</option>
-              <option value="Upupidae">Upupidae</option>
+                  <option value="Alcedinidae">Alcedinidae</option>
+                  <option value="Anatidae">Anatidae</option>
+                  <option value="Apodidae">Apodidae</option>
+                  <option value="Ardeidae">Ardeidae</option>
+                  <option value="Caprimulgidae">Caprimulgidae</option>
+                  <option value="Certhiidae">Certhiidae</option>
+                  <option value="Charadriidae">Charadriidae</option>
+                  <option value="Ciconiidae">Ciconiidae</option>
+                  <option value="Columbidae">Columbidae</option>
+                  <option value="Corvidae">Corvidae</option>
+                  <option value="Cuculidae">Cuculidae</option>
+                  <option value="Falconidae">Falconidae</option>
+                  <option value="Fringillidae">Fringillidae</option>
+                  <option value="Haematopodidae">Haematopodidae</option>
+                  <option value="Hirundinidae">Hirundinidae</option>
+                  <option value="Laniidae">Laniidae</option>
+                  <option value="Locustellidae">Locustellidae</option>
+                  <option value="Meropidae">Meropidae</option>
+                  <option value="Motacillidae">Motacillidae</option>
+                  <option value="Muscicapidae">Muscicapidae</option>
+                  <option value="Oriolidae">Oriolidae</option>
+                  <option value="Panuridae">Panuridae</option>
+                  <option value="Paridae">Paridae</option>
+                  <option value="Phasianidae">Phasianidae</option>
+                  <option value="Phylloscopidae">Phylloscopidae</option>
+                  <option value="Picidae">Picidae</option>
+                  <option value="Podicipedidae">Podicipedidae</option>
+                  <option value="Prunellidae">Prunellidae</option>
+                  <option value="Regulidae">Regulidae</option>
+                  <option value="Remizidae">Remizidae</option>
+                  <option value="Scolopacidae">Scolopacidae</option>
+                  <option value="Sittidae">Sittidae</option>
+                  <option value="Sturnidae">Sturnidae</option>
+                  <option value="Sylviidae">Sylviidae</option>
+                  <option value="Troglodytidae">Troglodytidae</option>
+                  <option value="Turdidae">Turdidae</option>
+                  <option value="Upupidae">Upupidae</option>
                 </>
               )}
               {filterType === "order" && (
                 <>
                   <option value="ACCIPITRIFORMES">ACCIPITRIFORMES</option>
-              <option value="ANSERIFORMES">ANSERIFORMES</option>
-              <option value="BUCEROTIFORMES">BUCEROTIFORMES</option>
-              <option value="CAPRIMULGIFORMES">CAPRIMULGIFORMES</option>
-              <option value="CHARADRIIFORMES">CHARADRIIFORMES</option>
-              <option value="CICONIIFORMES">CICONIIFORMES</option>
-              <option value="COLUMBIFORMES">COLUMBIFORMES</option>
-              <option value="CORACIIFORMES">CORACIIFORMES</option>
-              <option value="CUCULIFORMES">CUCULIFORMES</option>
-              <option value="FALCONIFORMES">FALCONIFORMES</option>
-              <option value="GRUIFORMES">GRUIFORMES</option>
-              <option value="PELECANIFORMES">PELECANIFORMES</option>
-              <option value="PICIFORMES">PICIFORMES</option>
-              <option value="PODICIPEDIFORMES">PODICIPEDIFORMES</option>
-              <option value="PROCELLARIIFORMES">PROCELLARIIFORMES</option>
+                  <option value="ANSERIFORMES">ANSERIFORMES</option>
+                  <option value="BUCEROTIFORMES">BUCEROTIFORMES</option>
+                  <option value="CAPRIMULGIFORMES">CAPRIMULGIFORMES</option>
+                  <option value="CHARADRIIFORMES">CHARADRIIFORMES</option>
+                  <option value="CICONIIFORMES">CICONIIFORMES</option>
+                  <option value="COLUMBIFORMES">COLUMBIFORMES</option>
+                  <option value="CORACIIFORMES">CORACIIFORMES</option>
+                  <option value="CUCULIFORMES">CUCULIFORMES</option>
+                  <option value="FALCONIFORMES">FALCONIFORMES</option>
+                  <option value="GRUIFORMES">GRUIFORMES</option>
+                  <option value="PELECANIFORMES">PELECANIFORMES</option>
+                  <option value="PICIFORMES">PICIFORMES</option>
+                  <option value="PODICIPEDIFORMES">PODICIPEDIFORMES</option>
+                  <option value="PROCELLARIIFORMES">PROCELLARIIFORMES</option>
                 </>
               )}
             </select>
+            <button className={styles.clearFilterButton} onClick={clearFilter}>
+              Clear Filter
+            </button>
           </div>
         )}
-        {isFilterActive && (
-          <button onClick={clearFilter}>Clear Filter</button>
-        )}
       </div>
-      {filteredBirds.map((bird) => (
-        <BirdCard
-          key={bird.id.toString()}
-          bird={bird}
-          onDelete={handleDeleteBird}
-          onToggleFavorite={onToggleFavorite}
-          isFavorite={favorites.includes(bird.id)}
-        />
-      ))}
-      {showBirdForm && <BirdForm onClose={handleCloseForm} addBird={addNewBird} />}
+
+      <div className={styles.cardContainer}>
+        {filteredBirds.map((bird) => (
+          <BirdCard
+            key={bird.id}
+            bird={bird}
+            isFavorite={favorites.includes(bird.id)}
+            onToggleFavorite={onToggleFavorite}
+            onDelete={() => handleDeleteBird(bird.id)}
+          />
+        ))}
+      </div>
+
       <button className={styles.addBirdButton} onClick={handleAddBird}>
         Add Bird
       </button>
+
+      {showBirdForm && (
+        <BirdForm onSubmit={addNewBird} onClose={handleCloseForm} />
+      )}
     </div>
   );
 };
